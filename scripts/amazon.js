0aee8449464737d1575685c3c14604e9e0a2e665
mark neil <markneilvtanap@gmail.com>
2024-06-22 10:10:43 +0800

    cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${product.id}"">
             Add to Cart
           </button>
         </div>
@@ -57,3 +57,32 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button)=> {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+     
+      let matchItem;
+
+      cart.forEach((item) => {
+
+        if (productId === item.id){
+          matchItem = item;
+        }
+      });
+
+      if (matchItem){
+        matchItem.quantity += 1;
+      }else{
+        cart.push(
+          {
+            id: productId,
+            quantity: 1
+          }
+        );
+      }
+
+    
+      console.log(cart);
+    });
+});;
